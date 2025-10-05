801be29654eafdc420fc9c0f250bdb2313b9c585
JennyXxie <xieyunjin0420@outlook.com>
2025-10-05 10:45:58 -0400

    delete everything first
    1

--- pages/ProductFetcher.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ProductFetcher from "./src/pages/ProductFetcher.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ProductFetcher />
-  </React.StrictMode>
-);
